9ad9770d0bed3a88cff7aae17940de846594dcbd
Lisa <ml.salto@alumnos.upm.es>
2023-08-19 13:55:44 +0200

    Terminado bboton cambio nivel y escena

--- Assets/Scripts/UI/CambioEscena.cs
+++ Assets/Scripts/UI/CambioEscena.cs
@@ -1,18 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CambioEscena : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void Cambio(string nombre)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene(nombre);
     }
 }
