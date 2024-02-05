3d855153ad08466c8731ea9ce95e1ebc89ade66f
Lisa <ml.salto@alumnos.upm.es>
2024-02-06 00:39:19 +0100

    demo juego 2

--- Assets/Scripts/Juego2/J2Nivel_1.cs
+++ Assets/Scripts/Juego2/J2Nivel_1.cs
@@ -13,8 +13,10 @@ public class J2Nivel_1 : MonoBehaviour
     [SerializeField] private TMP_Text texto;
 
     //private int[] vectorNum = { 1, 2, 3, 4, 5 };
+    private bool start;
     private int numParaSumar;
     public TriggerAcuario acuario;
+    public GameObject botonE;
 
     // Start is called before the first frame update
     void Start()
@@ -22,8 +24,9 @@ public class J2Nivel_1 : MonoBehaviour
         // creamos un numero aleatorio para el que tenemos que sumar  //
         System.Random random = new System.Random();
         numParaSumar = random.Next(1, 11); //numero del 1 al 10
-
-        texto.text = "El mar se está quedando sin peces por la contaminación. ¡¡¡¡ Ayudame a repoblarlos sumando !!!!";
+        start = false;
+        botonE.SetActive(true);
+        texto.text = "El mar se está quedando sin peces por la contaminación. \n ¡¡¡¡ Ayudame a repoblarlos sumando !!!!";
     }
 
     // Update is called once per frame
@@ -33,12 +36,32 @@ public class J2Nivel_1 : MonoBehaviour
         //El numero del nivel 1 es de 1-10, nivel 2 10-99, nivel 3 99-999, (solo con dec, cent, uni) 
         // suma
         //vectorNum = acuario.vectorNum;
-        texto.text = "Suma los peces para conseguir el número " + numParaSumar.ToString()  + "\n \n" + ConvertirVectorATexto(acuario.vectNumeros);
+        if (start == true)
+        {
+            botonE.SetActive(false);
+            texto.text = "Suma los peces para conseguir el número " + numParaSumar.ToString() + "\n \n" + ConvertirVectorATexto(acuario.vectNumeros, acuario.numPecesIn);
+
+            if (numParaSumar == acuario.sumaTot)
+            {
+                texto.text = "Suma los peces para conseguir el número " + numParaSumar.ToString() + "\n \n" + ConvertirVectorATexto(acuario.vectNumeros, acuario.numPecesIn) + "\n \n" + "CORRECTO  :)";
+            }
+
+            else if (numParaSumar < acuario.sumaTot)
+            {
+                texto.text = "Suma los peces para conseguir el número " + numParaSumar.ToString() + "\n \n" + ConvertirVectorATexto(acuario.vectNumeros, acuario.numPecesIn) + "\n \n" + "INCORRECTO  :(";
+            }
+        }
     }
 
+    // funcion para empezar juego 
+
+    public void empezarJuego()
+    {
+        start = true;
+    }
 
     // Funcion que convierte el vector de numeros a texto
-    static string ConvertirVectorATexto(int[] vector)
+    static string ConvertirVectorATexto(int[] vector, int size)
     {
         StringBuilder textoTmp = new StringBuilder();
         int suma = 0;
@@ -48,9 +71,10 @@ public class J2Nivel_1 : MonoBehaviour
         {
             // Agregar el primer elemento
             textoTmp.Append(vector[0]);
+            suma = vector[0];
 
             // Agregar los demás elementos con el formato "+ elemento"
-            for (int i = 1; i < vector.Length; i++)
+            for (int i = 1; i < size; i++)
             {
                 textoTmp.Append(" + " + vector[i]);
                 suma += vector[i];
--- Assets/Scripts/Juego2/TriggerAcuario.cs
+++ Assets/Scripts/Juego2/TriggerAcuario.cs
@@ -7,14 +7,17 @@ public class TriggerAcuario : MonoBehaviour
     // Start is called before the first frame update
     public int contador;
     private int i; //numero de numeros 
-    //public int[] vectorNum;
-    private int[] vectorNum = { 1, 2, 3, 4, 5 };
+    private int[] vectorNum;
+    private int suma;
+    //private int[] vectorNum = { 1, 2, 3, 4, 5 };
 
     void Start()
     {
         contador = 0;
         i = 0;
+        suma = 0;
         //int[] vectorNum = { 1, 2, 3, 4, 5 };
+        vectorNum = new int[10];
     }
 
     // Update is called once per frame
@@ -26,10 +29,16 @@ public class TriggerAcuario : MonoBehaviour
     //En el caso de que detecte colision pues añade la suma (y añadri que se ilumine tmb)
     private void OnTriggerEnter(Collider collision)
     {
-        contador = contador + int.Parse(collision.gameObject.tag);
-
+        //contador = contador + int.Parse(collision.gameObject.tag);
+        suma = 0;
         // actualizar el vector de los números (text a numeros)
         vectorNum[i] = int.Parse(collision.gameObject.tag);
+
+        for (int a = 0 ; a <= i; a++)
+        {
+            suma += vectorNum[a];
+        }
+
         i++;
     }
 
@@ -40,6 +49,20 @@ public class TriggerAcuario : MonoBehaviour
             return vectorNum;
         } 
     }
-    //private int num { return i; }
     
+    public int numPecesIn {
+        get
+        {
+            return i;
+        }
+    }
+
+    public int sumaTot
+    {
+        get
+        {
+            return suma;
+        }
+    }
+
 }
